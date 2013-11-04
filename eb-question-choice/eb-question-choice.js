891142f73de34db9d295af06d9efc22b9d2e233c
Berry de Vos <bdevos@gmail.com>
2013-11-04 12:29:34 +0100

    First implementation of the choice generic question type

--- eb-question-choice/eb-question-choice.js
+++ eb-question-choice/eb-question-choice.js
@@ -1,1 +1,35 @@
-console.log('Generic question: Choice, with added features');
+(function(window, angular, undefined) {'use strict';
+
+var ebQuestions = angular.module('ebQuestions', []);
+
+ebQuestions.directive('ebQuestionChoice', function () {
+  return {
+    restrict: 'E',
+    template: '<ul class="eb-question eb-question-choice">' +
+      '<li ng-repeat="choice in choices">' +
+      '<input type="radio" id="choice-{{ activity.id }}-{{ choice.id }}" name="answer-{{ activity.id }}" ' +
+      'ng-disabled="disabled" ' +
+      'value="{{ choice.id }}" ng-model="activity.value" ng-change="triggerEvent(choice.id)" />' +
+      '<label for="choice-{{ activity.id }}-{{ choice.id }}">{{ choice.value[0].text }}</label>' +
+      '</li></ul>',
+    replace: true,
+    scope: {
+      choices: '=',
+      activity: '=',
+      disabled: '=',
+      event: '@'
+    },
+    link: function postLink(scope) {
+      scope.triggerEvent = function(choiceId) {
+        if (scope.event !== undefined) {
+          scope.$emit(scope.event, {
+            activityId: scope.activity.id,
+            choiceId: choiceId
+          });
+        }
+      };
+    }
+  };
+});
+
+})(window, window.angular);
